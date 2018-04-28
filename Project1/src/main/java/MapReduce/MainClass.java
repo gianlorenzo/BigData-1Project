4d0efcfaed129246109717bcf4a66674ba802db6
gianlorenzo <gianlorenzo@gianlorenzo-NandG>
2018-04-28 12:03:16 +0200

    inizio scrittura main

---
+++ Project1/src/main/java/MapReduce/MainClass.java
@@ -0,0 +1,20 @@
+package MapReduce;
+
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+
+public class MainClass {
+
+	public static void main (String args[]) throws IOException {
+		
+		Job job = new Job(new Configuration(), "MainClass");
+		
+		job.setJarByClass(MainClass.class);
+
+	}
+
+}
---
+++ Project1/src/main/java/MapReduce/ReducerClass.java
@@ -0,0 +1,7 @@
+package MapReduce;
+
+public class ReducerClass {
+	
+	
+
+}
