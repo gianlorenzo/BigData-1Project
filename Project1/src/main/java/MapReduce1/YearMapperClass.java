2d8342d5b2794d6ce6c4f8304c9dfb4f9a919a03
user <user@user-VirtualBox>
2018-05-06 16:22:07 +0200

    lettura csv

--- Project1/src/main/java/MapReduce1/YearMapperClass.java
+++ Project1/src/main/java/MapReduce1/YearMapperClass.java
@@ -1,30 +1,37 @@
 package MapReduce1;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+//import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
 public class YearMapperClass extends Mapper<LongWritable, Text, Text, IntWritable> {
-/*
-	private static final IntWritable one = new IntWritable(1);
+
+/*	private static final IntWritable one = new IntWritable(1);
 	private Text word = new Text();
+*/	
+	
+	String year="";
+	String word="";
 
-	public void map(LongWritable key, Text value, Context context)
-			throws IOException, InterruptedException {
-
+	public void map(LongWritable key, Text value, OutputCollector<Text,Text> output, Context context)	throws IOException, InterruptedException {
+ 
 		String line = value.toString();
-		StringTokenizer tokenizer = new StringTokenizer(line);
-
-		while (tokenizer.hasMoreTokens()) {
-			word.set(tokenizer.nextToken());
-			context.write(word, one);
+		String[] results=line.split(",");
+		word=results[7];
+		year=results[8]; // è dato come timestamp, va convertito in data e estratto l'anno
+		output.collect(new Text(year), new Text(word));
+		/*
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				context.write(word, one);
+		*/
 		}
-
+		 
 	}
-*/
-}
