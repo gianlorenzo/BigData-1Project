bbd1c6dce04528a2c642daa25e064ffc354c7802
gianlorenzo <gianlorenzo@NandG>
2018-05-09 14:49:15 +0200

    prova

---
+++ Project1/src/main/java/MapReduce3/ProductReducerClass.java
@@ -0,0 +1,31 @@
+package MapReduce3;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class ProductReducerClass extends Reducer<Text, IntWritable, Text, IntWritable> {
+	
+	public void reduce(Text key, Iterator<IntWritable> values,
+			 OutputCollector<Text, IntWritable> output,
+			 Reporter reporter)
+			throws IOException {
+		
+		int i = 0;
+		
+		while(values.hasNext()) {
+			i++;
+		}
+		
+		output.collect(key, new IntWritable(i));
+		
+		
+	}
+
+}
