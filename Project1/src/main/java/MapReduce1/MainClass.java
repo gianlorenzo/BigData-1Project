bacd5f85062ce469543ae72e8234627fc741b167
user <user@user-VirtualBox>
2018-05-12 13:47:38 +0200

    update

--- Project1/src/main/java/MapReduce1/MainClass.java
+++ Project1/src/main/java/MapReduce1/MainClass.java
@@ -1,11 +1,10 @@
 package MapReduce1;
 
-import java.util.Calendar;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
+//import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
